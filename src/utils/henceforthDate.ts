600b12d92510ab8153cc794cf1255a527bb333dc
aryantiwari12 <aryantiwari1289@gmail.com>
2023-03-28 13:29:37 +0530

    bugs

--- src/utils/henceforthDate.ts
+++ src/utils/henceforthDate.ts
@@ -1,16 +1,17 @@
 const weekName = [
+    "Domingo",
     "Segunda-feira",
     "Terça-feira",
     "Quarta-feira",
     "Quinta-feira",
     "Sexta-feira",
     "Sábado",
-    "Domingo",
 ]
 const getWeekName = (index: number) => {
     console.log('getWeekName called', index);
+    console.log('getWeekName called', weekName[index]);
 
-    return weekName[index-1]
+    return weekName[index]
 }
 
 export default {
